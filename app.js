375af94dfb57f379f206bfc0175a59a9d6bf04ea
= <gs816912@gmail.com>
2020-01-08 21:06:05 +0530

    day 1 21:06 ist

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+//init git class
+const github =new GitHub;
+
 // search input
 const searchUser = document.getElementById('searchUser');
 
+// search input event listener
 searchUser.addEventListener('keyup',(e) =>{
+    // input value
     const userText = e.target.value;
     if(userText !==' '){
-        console.log(userText);
+        // Make http call
+        github.getUser(userText)
+        .then(data =>{
+            if(data.profile.meassage === 'Not Found'){
+                //show alert
+
+            }else{
+                //show profile
+
+            }
+        })
+    }else{
+        // Clear profile
+
     }
 });
-
-// search inpu event listener
 //searchUser.addEventListener()
